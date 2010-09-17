45af759437599c68c6e9eb0b2778af1186335ee7
krisztian.sallai@indgroup.eu <krisztian.sallai@indgroup.eu@73310840-744b-ed4c-61c9-fa74633f37e2>
2010-09-17 12:54:24 +0000

    Layout fixing on overview page  - if big description content is added

--- trunk/com.skzs.multiproperties.ui/src/com/skzs/multiproperties/ui/editors/OverviewPage.java
+++ trunk/com.skzs.multiproperties.ui/src/com/skzs/multiproperties/ui/editors/OverviewPage.java
@@ -1,6 +1,3 @@
-/*
- * Created on Nov 4, 2005
- */
 package com.skzs.multiproperties.ui.editors;
 
 import org.eclipse.core.runtime.IConfigurationElement;
@@ -12,9 +9,6 @@ import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
@@ -28,17 +22,16 @@ import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.ScrolledForm;
 import org.eclipse.ui.forms.widgets.Section;
 import org.eclipse.ui.forms.widgets.TableWrapData;
-import org.eclipse.ui.forms.widgets.TableWrapLayout;
 
 import com.skzs.multiproperties.base.model.Table;
 import com.skzs.multiproperties.ui.Activator;
+import com.skzs.multiproperties.ui.util.LayoutFactory;
 
 public class OverviewPage extends FormPage
 {
 
 	private final Editor editor;
 	//GeneralInfoSection
-	private Label version_label;
 	private Text name;
 	private Text description;
 	private Combo handler;
@@ -86,28 +79,18 @@ public class OverviewPage extends FormPage
 	private void fillBody(final IManagedForm managedForm, final FormToolkit toolkit)
 	{
 		final Composite body = managedForm.getForm().getBody();
-		final TableWrapLayout layout = new TableWrapLayout();
-		layout.topMargin = 0;
-		layout.bottomMargin = 0;
-		layout.leftMargin = 0;
-		layout.rightMargin = 0;
-		layout.numColumns = 2;
-		body.setLayout(layout);
+		body.setLayout(LayoutFactory.createTableWrapLayout(2, 0, 0));
+
 		// Left container
 		final Composite left = toolkit.createComposite(body);
-		GridLayout gridlayout = new GridLayout();
-		gridlayout.marginHeight = 10;
-		gridlayout.marginWidth = 5;
-		left.setLayout(gridlayout);
+		left.setLayout(LayoutFactory.createTableWrapLayout());
 		left.setLayoutData(new TableWrapData(TableWrapData.FILL_GRAB));
 		fillGeneralInfoSection(left, toolkit);
 		fillHandlerSection(left, toolkit);
+
 		// Right container
 		final Composite right = toolkit.createComposite(body);
-		gridlayout = new GridLayout();
-		gridlayout.marginHeight = 10;
-		gridlayout.marginWidth = 5;
-		right.setLayout(gridlayout);
+		right.setLayout(LayoutFactory.createTableWrapLayout());
 		right.setLayoutData(new TableWrapData(TableWrapData.FILL_GRAB));
 		fillNavigationSection(right, toolkit);
 		fillStatisticsSection(right, toolkit);
@@ -118,19 +101,14 @@ public class OverviewPage extends FormPage
 		final Section section = toolkit.createSection(parent, Section.TITLE_BAR | Section.DESCRIPTION);
 		section.setText("General Information");
 		section.setDescription("This section describes general information about this multiproperties.");
-		section.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL | GridData.GRAB_VERTICAL | GridData.FILL_BOTH));
-		section.setLayout(new FillLayout());
+		section.setLayoutData(new TableWrapData(TableWrapData.FILL_GRAB));
 		final Composite composite = toolkit.createComposite(section);
-		composite.setLayout(new GridLayout(2, false));
+		composite.setLayout(LayoutFactory.createTableWrapLayout(2, 0, 0));
 		section.setClient(composite);
-		// Version
-		toolkit.createLabel(composite, "Version: ", SWT.NONE).setForeground(toolkit.getColors().getColor(IFormColors.TITLE));
-		version_label = toolkit.createLabel(composite, editor.getTable().getVersion(), SWT.NONE);
-		version_label.setLayoutData(new GridData(GridData.FILL_HORIZONTAL | GridData.GRAB_HORIZONTAL));
 		// Name
 		toolkit.createLabel(composite, "Name: ", SWT.NONE).setForeground(toolkit.getColors().getColor(IFormColors.TITLE));
 		name = toolkit.createText(composite, editor.getTable().getName(), SWT.NONE);
-		name.setLayoutData(new GridData(GridData.FILL_HORIZONTAL | GridData.GRAB_HORIZONTAL));
+		name.setLayoutData(new TableWrapData(TableWrapData.FILL_GRAB));
 		name.addModifyListener(new ModifyListener() {
 
 			public void modifyText(final ModifyEvent arg0)
@@ -140,8 +118,10 @@ public class OverviewPage extends FormPage
 		});
 		// Description
 		toolkit.createLabel(composite, "Description: ", SWT.NONE).setForeground(toolkit.getColors().getColor(IFormColors.TITLE));
-		description = toolkit.createText(composite, editor.getTable().getDescription(), SWT.MULTI);
-		description.setLayoutData(new GridData(GridData.FILL_BOTH | GridData.GRAB_HORIZONTAL | GridData.GRAB_VERTICAL));
+		description = toolkit.createText(composite, editor.getTable().getDescription(), SWT.MULTI | SWT.WRAP | SWT.H_SCROLL | SWT.V_SCROLL);
+		final TableWrapData tableWrapData = new TableWrapData(TableWrapData.FILL_GRAB);
+		tableWrapData.heightHint = 160;
+		description.setLayoutData(tableWrapData);
 		description.addModifyListener(new ModifyListener() {
 
 			public void modifyText(final ModifyEvent arg0)
@@ -156,15 +136,14 @@ public class OverviewPage extends FormPage
 		final Section section = toolkit.createSection(parent, Section.TITLE_BAR | Section.DESCRIPTION);
 		section.setText("Handler");
 		section.setDescription("The handlers are able to produce different kind of outputs from the multiproperties.");
-		section.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL | GridData.GRAB_VERTICAL | GridData.FILL_BOTH));
-		section.setLayout(new FillLayout());
+		section.setLayoutData(new TableWrapData(TableWrapData.FILL_GRAB));
 		final Composite composite = toolkit.createComposite(section);
-		composite.setLayout(new GridLayout(2, false));
+		composite.setLayout(LayoutFactory.createTableWrapLayout(2, 0, 0));
 		section.setClient(composite);
 		// Handler
 		toolkit.createLabel(composite, "Handler: ", SWT.NONE).setForeground(toolkit.getColors().getColor(IFormColors.TITLE));
 		handler = new Combo(composite, SWT.BORDER | SWT.READ_ONLY);
-		handler.setLayoutData(new GridData(GridData.FILL_HORIZONTAL | GridData.GRAB_HORIZONTAL));
+		handler.setLayoutData(new TableWrapData(TableWrapData.FILL));
 
 		final IExtensionRegistry reg = Platform.getExtensionRegistry();
 		final IConfigurationElement[] extensions = reg.getConfigurationElementsFor("com.skzs.multiproperties.handler");
@@ -230,9 +209,9 @@ public class OverviewPage extends FormPage
 		final Section section = toolkit.createSection(parent, Section.TITLE_BAR | Section.DESCRIPTION);
 		section.setText("Navigation");
 		section.setDescription("This section describes the pages of the editor shortly.");
-		section.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL | GridData.GRAB_VERTICAL | GridData.FILL_BOTH));
+		section.setLayoutData(new TableWrapData(TableWrapData.FILL_GRAB));
 		final Composite composite = toolkit.createComposite(section);
-		composite.setLayout(new GridLayout(2, false));
+		composite.setLayout(LayoutFactory.createTableWrapLayout(2, 0, 0));
 		section.setClient(composite);
 		// Text
 		final FormText text = toolkit.createFormText(composite, true);
@@ -255,7 +234,7 @@ public class OverviewPage extends FormPage
 				editor.setActivePage(e.getHref().toString());
 			}
 		});
-		text.setLayoutData(new GridData(GridData.FILL_HORIZONTAL | GridData.GRAB_HORIZONTAL));
+		text.setLayoutData(new TableWrapData(TableWrapData.FILL));
 	}
 
 	private void fillStatisticsSection(final Composite parent, final FormToolkit toolkit)
@@ -263,25 +242,20 @@ public class OverviewPage extends FormPage
 		final Section section = toolkit.createSection(parent, Section.TITLE_BAR | Section.DESCRIPTION);
 		section.setText("Statistics");
 		section.setDescription("This section gives statistical information about this multiproperties.");
-		section.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL | GridData.GRAB_VERTICAL | GridData.FILL_BOTH));
+		section.setLayoutData(new TableWrapData(TableWrapData.FILL_GRAB));
 		final Composite composite = toolkit.createComposite(section);
-		composite.setLayout(new GridLayout(2, false));
+		composite.setLayout(LayoutFactory.createTableWrapLayout(2, 0, 0));
 		section.setClient(composite);
 		// Counters
 		toolkit.createLabel(composite, "Count of properties files: ", SWT.NONE).setForeground(toolkit.getColors().getColor(IFormColors.TITLE));
 		count_of_properties = toolkit.createLabel(composite, "", SWT.NONE);
-		count_of_properties.setLayoutData(new GridData(GridData.FILL_HORIZONTAL | GridData.GRAB_HORIZONTAL));
 		toolkit.createLabel(composite, "Count of property lines: ", SWT.NONE).setForeground(toolkit.getColors().getColor(IFormColors.TITLE));
 		count_of_keys = toolkit.createLabel(composite, "", SWT.NONE);
-		count_of_keys.setLayoutData(new GridData(GridData.FILL_HORIZONTAL | GridData.GRAB_HORIZONTAL));
 		toolkit.createLabel(composite, "Count of disabled property lines: ", SWT.NONE).setForeground(toolkit.getColors().getColor(IFormColors.TITLE));
 		count_of_disabled = toolkit.createLabel(composite, "", SWT.NONE);
-		count_of_disabled.setLayoutData(new GridData(GridData.FILL_HORIZONTAL | GridData.GRAB_HORIZONTAL));
 		toolkit.createLabel(composite, "Count of comment lines: ", SWT.NONE).setForeground(toolkit.getColors().getColor(IFormColors.TITLE));
 		count_of_comments = toolkit.createLabel(composite, "", SWT.NONE);
-		count_of_comments.setLayoutData(new GridData(GridData.FILL_HORIZONTAL | GridData.GRAB_HORIZONTAL));
 		toolkit.createLabel(composite, "Count of empty lines: ", SWT.NONE).setForeground(toolkit.getColors().getColor(IFormColors.TITLE));
 		count_of_emptyline = toolkit.createLabel(composite, "", SWT.NONE);
-		count_of_emptyline.setLayoutData(new GridData(GridData.FILL_HORIZONTAL | GridData.GRAB_HORIZONTAL));
 	}
 }
