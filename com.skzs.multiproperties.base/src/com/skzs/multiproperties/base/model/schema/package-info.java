e752e5bd3cbe734562ce5bd3427137c8fed4de33
krisztian.sallai@indgroup.eu <krisztian.sallai@indgroup.eu@73310840-744b-ed4c-61c9-fa74633f37e2>
2011-04-18 23:00:38 +0000

    Using JAXB 2.2.3 release

--- com.skzs.multiproperties.base/src/com/skzs/multiproperties/base/model/schema/package-info.java
+++ com.skzs.multiproperties.base/src/com/skzs/multiproperties/base/model/schema/package-info.java
@@ -1,2 +1,2 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "multiproperties.indgroup.eu", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+@javax.xml.bind.annotation.XmlSchema(namespace = "com.skzs.multiproperties", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package com.skzs.multiproperties.base.model.schema;
