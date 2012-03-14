1bd5259f8982d3eb6bfae7858b2eaa8cece2b0fb
skazsi@gmail.com <skazsi@gmail.com@73310840-744b-ed4c-61c9-fa74633f37e2>
2012-03-14 14:38:06 +0000

    Adding further unit tests

---
+++ trunk/hu.skzs.multiproperties.base.test/src/hu/skzs/multiproperties/base/model/ModelTests.java
@@ -0,0 +1,12 @@
+package hu.skzs.multiproperties.base.model;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CommentRecordTest.class })
+public class ModelTests
+{
+
+}
