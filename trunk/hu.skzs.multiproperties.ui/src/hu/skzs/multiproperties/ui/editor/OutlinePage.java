4292e4aa21eaf27430c458b27f50a294f930c004
skazsi@gmail.com <skazsi@gmail.com@73310840-744b-ed4c-61c9-fa74633f37e2>
2012-04-25 15:56:52 +0000

    Supporting default value http://code.google.com/a/eclipselabs.org/p/multiproperties/issues/detail?id=11
    Outline view is updated

--- trunk/hu.skzs.multiproperties.ui/src/hu/skzs/multiproperties/ui/editor/OutlinePage.java
+++ trunk/hu.skzs.multiproperties.ui/src/hu/skzs/multiproperties/ui/editor/OutlinePage.java
@@ -1,91 +1,70 @@
 package hu.skzs.multiproperties.ui.editor;
 
 import hu.skzs.multiproperties.base.model.PropertyRecord;
+import hu.skzs.multiproperties.base.model.Table;
 import hu.skzs.multiproperties.ui.Activator;
 import hu.skzs.multiproperties.ui.Messages;
 import hu.skzs.multiproperties.ui.preferences.PreferenceConstants;
+import hu.skzs.multiproperties.ui.util.LayoutFactory;
 
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.viewers.CellLabelProvider;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ControlAdapter;
 import org.eclipse.swt.events.ControlEvent;
-import org.eclipse.swt.events.ControlListener;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
-import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.IPageSite;
+import org.eclipse.ui.part.Page;
 import org.eclipse.ui.views.contentoutline.IContentOutlinePage;
 
 /**
- * The <code>OutlinePage</code> vizualizes the currently selected {@link PropertyRecord} if there is
+ * The <code>OutlinePage</code> visualizes the currently selected {@link PropertyRecord} if there is
  * exactly one this typed selection.
  * @author sallai
  */
-public class OutlinePage implements IContentOutlinePage, ISelectionListener
+public class OutlinePage extends Page implements IContentOutlinePage, ISelectionListener
 {
 
-	private Composite composite;
+	private Composite control;
 	private Label keylabel;
-	private Table table;
+	private TableViewer tableViewer;
+
+	/**
+	 * The <code>table</code> member hold a reference to the {@link Table} instance.
+	 */
+	private final Table table;
+
+	/**
+	 * Default constructor
+	 * @param table the given {@link Table} instance
+	 */
+	public OutlinePage(final Table table)
+	{
+		this.table = table;
+	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.part.IPage#createControl(org.eclipse.swt.widgets.Composite)
+	 * @see org.eclipse.ui.part.Page#init(org.eclipse.ui.part.IPageSite)
 	 */
-	public void createControl(final Composite parent)
+	@Override
+	public void init(final IPageSite pageSite)
 	{
-		composite = new Composite(parent, SWT.NONE);
-		final GridLayout gridlayout = new GridLayout(1, false);
-		gridlayout.marginHeight = 0;
-		gridlayout.marginWidth = 0;
-		composite.setLayout(gridlayout);
-		keylabel = new Label(composite, SWT.NONE);
-		keylabel.setText(""); //$NON-NLS-1$
-		GridData griddata = new GridData(GridData.FILL_HORIZONTAL | GridData.GRAB_HORIZONTAL);
-		keylabel.setLayoutData(griddata);
-		table = new Table(composite, SWT.FULL_SELECTION);
-		griddata = new GridData(GridData.FILL_BOTH | GridData.GRAB_HORIZONTAL | GridData.GRAB_VERTICAL);
-		table.setLayoutData(griddata);
-		TableColumn tc = new TableColumn(table, SWT.NONE);
-		tc.setText(Messages.getString("outline.column")); //$NON-NLS-1$
-		tc.setWidth(Activator.getDefault().getPreferenceStore().getInt(PreferenceConstants.OUTLINE_COLUMN_WIDTH));
-		tc.addControlListener(new ControlListener() {
-
-			public void controlMoved(final ControlEvent e)
-			{
-			}
-
-			public void controlResized(final ControlEvent e)
-			{
-				final TableColumn tablecolumn = (TableColumn) e.getSource();
-				Activator.getDefault().getPreferenceStore().setValue(PreferenceConstants.OUTLINE_COLUMN_WIDTH, tablecolumn.getWidth());
-			}
-		});
-		tc = new TableColumn(table, SWT.NONE);
-		tc.setText(Messages.getString("outline.value")); //$NON-NLS-1$
-		tc.setWidth(Activator.getDefault().getPreferenceStore().getInt(PreferenceConstants.OUTLINE_VALUE_WIDTH));
-		tc.addControlListener(new ControlListener() {
-
-			public void controlMoved(final ControlEvent e)
-			{
-			}
-
-			public void controlResized(final ControlEvent e)
-			{
-				final TableColumn tablecolumn = (TableColumn) e.getSource();
-				Activator.getDefault().getPreferenceStore().setValue(PreferenceConstants.OUTLINE_VALUE_WIDTH, tablecolumn.getWidth());
-			}
-		});
-		table.setHeaderVisible(true);
+		super.init(pageSite);
 
 		// Registering the selection listener
 		PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService().addSelectionListener(this);
@@ -93,29 +72,138 @@ public class OutlinePage implements IContentOutlinePage, ISelectionListener
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.part.IPage#dispose()
+	 * @see org.eclipse.ui.part.Page#dispose()
 	 */
+	@Override
 	public void dispose()
 	{
 		// Unregistering the selection listener
 		PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService().removeSelectionListener(this);
+
+		super.dispose();
+	}
+
+	/**
+	 * Sets the Table
+	 * @param table
+	 */
+	public void setTable(final Table table)
+	{
+
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.part.IPage#getControl()
+	 * @see org.eclipse.ui.part.Page#createControl(org.eclipse.swt.widgets.Composite)
 	 */
+	@Override
+	public void createControl(final Composite parent)
+	{
+		// Creating the control
+		control = new Composite(parent, SWT.NONE);
+		control.setLayout(LayoutFactory.createGridLayout(1, 0, 0));
+
+		// Key label
+		keylabel = new Label(control, SWT.NONE);
+		keylabel.setText(""); //$NON-NLS-1$
+		keylabel.setLayoutData(new GridData(GridData.FILL_HORIZONTAL | GridData.GRAB_HORIZONTAL));
+
+		// Table viewer
+		tableViewer = new TableViewer(control, SWT.FULL_SELECTION);
+		tableViewer.getTable().setLayoutData(
+				new GridData(GridData.FILL_BOTH | GridData.GRAB_HORIZONTAL | GridData.GRAB_VERTICAL));
+		tableViewer.getTable().setLinesVisible(true);
+		tableViewer.getTable().setHeaderVisible(true);
+		tableViewer.setContentProvider(new OutlineContentProvider(table));
+
+		// Columns
+		createTableViewerColumn(new ColumnLabelProvider()
+		{
+			@Override
+			public String getText(final Object element)
+			{
+				final OutlineInputElement outlineInputElement = (OutlineInputElement) element;
+				return outlineInputElement.getColumn().getName();
+			}
+		}, Messages.getString("outline.column"), PreferenceConstants.OUTLINE_COLUMN_WIDTH); //$NON-NLS-1$
+		createTableViewerColumn(new OutlineValueColumnLabelProvider(),
+				Messages.getString("outline.value"), PreferenceConstants.OUTLINE_VALUE_WIDTH); //$NON-NLS-1$
+	}
+
+	/**
+	 * Creates a new {@link TableViewerColumn}
+	 * @param labelProvider the desired column {@link CellLabelProvider}
+	 * @param columnText the desired column text
+	 * @param columnWidthConst the constant which can be used to store the width of the column in the preference store
+	 */
+	private void createTableViewerColumn(final CellLabelProvider labelProvider, final String columnText,
+			final String columnWidthConst)
+	{
+		final IPreferenceStore preferenceStore = Activator.getDefault().getPreferenceStore();
+		final TableViewerColumn valueTableViewerColumn = new TableViewerColumn(tableViewer, SWT.NONE);
+		valueTableViewerColumn.setLabelProvider(labelProvider);
+		valueTableViewerColumn.getColumn().setText(columnText);
+		valueTableViewerColumn.getColumn().setWidth(preferenceStore.getInt(columnWidthConst));
+		valueTableViewerColumn.getColumn().addControlListener(new ControlAdapter()
+		{
+			@Override
+			public void controlResized(final ControlEvent e)
+			{
+				final TableColumn tablecolumn = (TableColumn) e.getSource();
+				preferenceStore.setValue(columnWidthConst, tablecolumn.getWidth());
+			}
+		});
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.ISelectionListener#selectionChanged(org.eclipse.ui.IWorkbenchPart, org.eclipse.jface.viewers.ISelection)
+	 */
+	public void selectionChanged(final IWorkbenchPart part, final ISelection selection)
+	{
+		String key = ""; //$NON-NLS-1$
+		PropertyRecord propertyRecord = null;
+		try
+		{
+			if (!(part instanceof Editor) || !(selection instanceof IStructuredSelection) || selection.isEmpty())
+				return;
+
+			final IStructuredSelection structuredSelection = (IStructuredSelection) selection;
+			if (structuredSelection.size() > 1)
+				return;
+
+			if (!(structuredSelection.getFirstElement() instanceof PropertyRecord))
+				return;
+
+			propertyRecord = (PropertyRecord) structuredSelection.getFirstElement();
+			key = propertyRecord.getValue();
+		}
+		finally
+		{
+			keylabel.setText(key);
+			tableViewer.setInput(propertyRecord);
+			tableViewer.refresh();
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.part.Page#getControl()
+	 */
+	@Override
 	public Control getControl()
 	{
-		return composite;
+		return control;
 	}
 
 	/*
 	 * (non-Javadoc)
 	 * @see org.eclipse.ui.part.IPage#setFocus()
 	 */
+	@Override
 	public void setFocus()
 	{
+		tableViewer.getTable().setFocus();
 	}
 
 	/*
@@ -157,42 +245,10 @@ public class OutlinePage implements IContentOutlinePage, ISelectionListener
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.part.IPage#setActionBars(org.eclipse.ui.IActionBars)
+	 * @see org.eclipse.ui.part.Page#setActionBars(org.eclipse.ui.IActionBars)
 	 */
+	@Override
 	public void setActionBars(final IActionBars actionBars)
 	{
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.ISelectionListener#selectionChanged(org.eclipse.ui.IWorkbenchPart, org.eclipse.jface.viewers.ISelection)
-	 */
-	public void selectionChanged(IWorkbenchPart part, ISelection selection)
-	{
-		keylabel.setText(""); //$NON-NLS-1$
-		table.removeAll();
-
-		if (!(part instanceof Editor) || !(selection instanceof IStructuredSelection) || selection.isEmpty())
-			return;
-
-		Editor editor = (Editor) part;
-		IStructuredSelection structuredSelection = (IStructuredSelection) selection;
-		if (structuredSelection.size() > 1)
-			return;
-
-		if (!(structuredSelection.getFirstElement() instanceof PropertyRecord))
-			return;
-
-		final PropertyRecord propertyRecord = (PropertyRecord) structuredSelection.getFirstElement();
-		keylabel.setText(propertyRecord.getValue());
-		for (int i = 0; i < editor.getTable().getColumns().size(); i++)
-		{
-			if (propertyRecord.getColumnValue(editor.getTable().getColumns().get(i)) != null)
-			{
-				final TableItem tableitem = new TableItem(table, SWT.NONE);
-				tableitem.setText(0, editor.getTable().getColumns().get(i).getName());
-				tableitem.setText(1, propertyRecord.getColumnValue(editor.getTable().getColumns().get(i)));
-			}
-		}
 	}
 }
---
+++ trunk/hu.skzs.multiproperties.ui/src/hu/skzs/multiproperties/ui/editor/OutlineValueColumnLabelProvider.java
@@ -0,0 +1,77 @@
+package hu.skzs.multiproperties.ui.editor;
+
+import hu.skzs.multiproperties.base.model.PropertyRecord;
+import hu.skzs.multiproperties.ui.Activator;
+import hu.skzs.multiproperties.ui.preferences.PreferenceConstants;
+
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.resource.StringConverter;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.Display;
+
+/**
+ * The <code>OutlineValueColumnLabelProvider</code> is the label provider for the outline page value column.
+ * @author sallai
+ */
+public class OutlineValueColumnLabelProvider extends ColumnLabelProvider
+{
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.jface.viewers.ColumnLabelProvider#getText(java.lang.Object)
+	 */
+	@Override
+	public String getText(final Object element)
+	{
+		final OutlineInputElement outlineInputElement = (OutlineInputElement) element;
+		final PropertyRecord propertyRecord = outlineInputElement.getPropertyRecord();
+
+		// Value
+		final String value = propertyRecord.getColumnValue(outlineInputElement.getColumn());
+		if (value != null)
+			return value;
+
+		// Default value
+		final String defaultValue = propertyRecord.getDefaultColumnValue();
+		if (defaultValue != null)
+			return defaultValue;
+
+		return ""; //$NON-NLS-1$
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.jface.viewers.ColumnLabelProvider#getForeground(java.lang.Object)
+	 */
+	@Override
+	public Color getForeground(final Object element)
+	{
+		final IPreferenceStore store = Activator.getDefault().getPreferenceStore();
+		final OutlineInputElement outlineInputElement = (OutlineInputElement) element;
+		final PropertyRecord propertyRecord = outlineInputElement.getPropertyRecord();
+
+		// Disabled
+		if (propertyRecord.isDisabled())
+			return parseColor(store.getString(PreferenceConstants.COLOR_DISABLED_PROPERTY_FOREGROUND));
+		else
+		{
+			if (propertyRecord.getColumnValue(outlineInputElement.getColumn()) != null)
+				return parseColor(store.getString(PreferenceConstants.COLOR_PROPERTY_FOREGROUND));
+			else if (propertyRecord.getDefaultColumnValue() != null)
+				return parseColor(store.getString(PreferenceConstants.COLOR_PROPERTY_DEFAULTVALUE_FOREGROUND));
+		}
+		return null;
+	}
+
+	/**
+	 * Returns a newly constructed {@link Color} instance based on the given <code>value</code> holding a <code>R,G,B</code> value.
+	 * @param value the given <code>R,G,B</code> value
+	 * @return a newly constructed {@link Color}
+	 */
+	private Color parseColor(final String value)
+	{
+		// TODO: The created Color instances should be disposed after the usage. Some color registry should be used maybe.
+		return new Color(Display.getDefault(), StringConverter.asRGB(value));
+	}
+}
