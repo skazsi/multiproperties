b15f164f942390cc610adcdfda8b41431af1ad86
skazsi@gmail.com <skazsi@gmail.com@73310840-744b-ed4c-61c9-fa74633f37e2>
2012-06-21 20:21:33 +0000

    http://code.google.com/a/eclipselabs.org/p/multiproperties/issues/detail?id=18 Exception is thrown when the Java handler configuration wizard is canceled

--- hu.skzs.multiproperties.base/src/hu/skzs/multiproperties/base/api/IHandlerConfigurator.java
+++ hu.skzs.multiproperties.base/src/hu/skzs/multiproperties/base/api/IHandlerConfigurator.java
@@ -19,14 +19,14 @@ public interface IHandlerConfigurator
 	 * button on the <strong>Columns</strong> page for the currently selected {@link Column}.
 	 * <p>The plugin provides a {@link Shell} instance to help representing any dialog or wizard by the handler, and it also
 	 * provides the previous <code>configuration</code> String object.</p>
-	 * <p>The returned String value is the modified handler configuration information.</p>
+	 * <p>The returned String value is the modified handler configuration information, or <code>null</code> which means the configuration is cancelled.</p>
 	 * <p>The <code>configuration</code> is String based information which is understandable by the handler only,
 	 * and specifies how the handler should write out the particular {@link Column}. 
 	 * The MultiProperties plugin guarantees only storing this String for each {@link Column} in the MultiProperties XML file.</p> 
 	 *  
 	 * @param shell the given {@link Shell}
 	 * @param configuration the current configuration String for the current {@link Column} object
-	 * @return the modified configuration String
+	 * @return the modified configuration String or <code>null</code>
 	 * @throws HandlerException
 	 */
 	public String configure(Shell shell, String configuration) throws HandlerException;
