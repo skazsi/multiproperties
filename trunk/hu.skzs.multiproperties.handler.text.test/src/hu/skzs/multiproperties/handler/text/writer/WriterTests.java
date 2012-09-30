53fff15cd84ff0e035f9dad60c5d71daa32148f4
skazsi@gmail.com <skazsi@gmail.com@73310840-744b-ed4c-61c9-fa74633f37e2>
2012-09-30 12:44:32 +0000

    Initial version of text handler

---
+++ trunk/hu.skzs.multiproperties.handler.text.test/src/hu/skzs/multiproperties/handler/text/writer/WriterTests.java
@@ -0,0 +1,12 @@
+package hu.skzs.multiproperties.handler.text.writer;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ WriterFactoryTest.class })
+public class WriterTests
+{
+
+}
